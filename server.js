a608d83f6f1f55523de48b5882ab5a75441a7f41
kezlya-tim <myhappiness1979@gmail.com>
2025-06-12 20:37:52 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public')); // 'public' — папка с HTML/CSS
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
